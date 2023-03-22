0c43b4e771a744f8365c00740bfc3e6d8e4d0d24
Jemimah Olufemi <jemimah.olufemi@wcsch.com>
2023-03-22 19:19:36 +0000

    remove 3d files

--- core/cell.py
+++ core/cell.py
@@ -1,5 +1,7 @@
 from typing import Any, cast, Dict, Hashable, List, Optional
 
+from core.distances import Distances
+
 class Cell:
        
     @property
@@ -45,6 +47,10 @@ class Cell:
     def __init__(self, row: int, column: int):
         self._row = row
         self._column = column
+        self._east = None
+        self._west = None
+        self._south = None
+        self._north = None
         self.links = {}
     
     def link(self, cell, bidi=True) -> 'Cell':
@@ -67,7 +73,7 @@ class Cell:
     def is_linked(self, cell: "Cell") -> bool:
         return cell in self.links.keys()
     
-    
+    @property
     def neighbours(self):
         list = []
         self.north and list.append(self.north)
@@ -75,6 +81,23 @@ class Cell:
         self.east and list.append(self.east)
         self.west and list.append(self.west)
         return list
+    
+    @property
+    def distances(self) -> Distances:
+        distances = Distances(self)
+        frontier = [self]
+
+        while len(frontier) > 0:
+            new_frontier = []
+            for cell in frontier:
+                for linked_cell in cell.links:
+                    if distances[linked_cell] is None and distances[cell] is not None:
+                        distances[linked_cell] = cast(int, distances[cell]) + 1
+                        new_frontier.append(linked_cell)
+            frontier = new_frontier
+
+        return distances
+
     
     def __eq__(self, other_cell: Optional["Cell"]) -> bool:       # type: ignore
         if not self.is_cell(other_cell) or other_cell is None:
@@ -84,9 +107,12 @@ class Cell:
     def is_cell(self, cell: Any) -> bool:
         return isinstance(cell, Cell)
     
+    def __iadd__(self, cell: "Cell") -> "Cell":
+        """
+        Overload for the += operator with the link method
+        """
+        self.link(cell)
+        return self
     
     def __hash__(self):
         return hash((self.row, self.column))
-    
-    
-    
